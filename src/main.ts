f5b95eef3464fe6f6a7dde12c4fb3607f79ec66e
masud2005 <masud20012005@gmail.com>
2026-05-24 10:52:19 +0600

    feat: enable CORS for all origins with support for credentials and standard methods

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: true,
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+        credentials: true,
+    });
     app.use(cookieParser());
     app.useGlobalPipes(
         new ValidationPipe({
