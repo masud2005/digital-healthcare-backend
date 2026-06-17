2fa0983390c2bbd07baff350cfeaa469ce2d077c
masud2005 <masud20012005@gmail.com>
2026-06-17 14:19:56 +0600

    chore: remove testimonial avatar and published status migration script

--- prisma/migrations/20260617000000_add_testimonial_avatar_and_published/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "testimonials" ADD COLUMN "avatar_id" TEXT,
-ADD COLUMN "is_published" BOOLEAN NOT NULL DEFAULT true;
-
--- CreateIndex
-CREATE UNIQUE INDEX "testimonials_avatar_id_key" ON "testimonials"("avatar_id");
-
--- AddForeignKey
-ALTER TABLE "testimonials" ADD CONSTRAINT "testimonials_avatar_id_fkey" FOREIGN KEY ("avatar_id") REFERENCES "Attachment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
