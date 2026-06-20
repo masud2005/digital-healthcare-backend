e4a06acf2cd1c6d93c37d5441c6a0b7a3b542c2b
masud2005 <masud20012005@gmail.com>
2026-06-20 10:44:42 +0600

    patient manage single assessment get api added

--- src/main/(admin)/patient-manage/patient-manage.module.ts
+++ src/main/(admin)/patient-manage/patient-manage.module.ts
@@ -1,12 +1,13 @@
 import { PrismaModule } from "@global/prisma/prisma.module";
 import { StorageModule } from "@global/storage/storage.module";
+import { AssessmentSubmissionModule } from "@main/(patient)/assessment-submission/assessment-submission.module";
 import { Module } from "@nestjs/common";
 import { PatientManageController } from "./patient-manage.controller";
 import { PatientManageRepository } from "./patient-manage.repository";
 import { PatientManageService } from "./patient-manage.service";
 
 @Module({
-    imports: [PrismaModule, StorageModule],
+    imports: [PrismaModule, StorageModule, AssessmentSubmissionModule],
     controllers: [PatientManageController],
     providers: [PatientManageService, PatientManageRepository],
 })
